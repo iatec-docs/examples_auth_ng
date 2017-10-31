76152665ec1ed8a48bcf3643409718bb43ee8814
Andre Guilherme <andre.guilherme@iatec.com>
2017-10-31 18:22:34 -0200

    Add API call

--- using_angular-oauth2-oidc/src/app/app.component.ts
+++ using_angular-oauth2-oidc/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Headers } from '@angular/http';
 import { OAuthService } from 'angular-oauth2-oidc';
 import { JwksValidationHandler } from 'angular-oauth2-oidc';
 import { authConfig } from './auth.config';
@@ -8,7 +9,8 @@ import { authConfig } from './auth.config';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  constructor(private oauthService: OAuthService) {
+  apiresponse = "API not called yet.";
+  constructor(private oauthService: OAuthService, private http: Http) {
     this.configureWithNewConfigApi();
   }
   private configureWithNewConfigApi() {
@@ -25,10 +27,23 @@ export class AppComponent {
     this.oauthService.logOut();
   }
 
+  public callApi() {
+    let headers = new Headers({
+      "Authorization": "Bearer " + this.oauthService.getAccessToken()
+    });
+    this.http.get('http://localhost:4000/demo/test', { headers: headers })
+      .subscribe(resp => {
+        debugger;
+        if (resp.status == 0) this.apiresponse = "Could not connect to the API.";
+        else this.apiresponse = `API response status:${resp.status} ${resp.statusText}\n\n${resp.json()}`;
+      }, err => {
+        this.apiresponse = `Error: ${err}`;
+      });
+  }
+  
   public get name() {
     let claims: any = this.oauthService.getIdentityClaims();
     if (!claims) return 'Anonymous User';
     return claims.name;
   }
-
 }
